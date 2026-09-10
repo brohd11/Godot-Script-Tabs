496b2bc6bcf9a4e1f8e115e1d8616889f6693e94
brohd <brohdielr@proton.me>
2026-09-10 02:12:59 -0700

    restructure utils

--- src/utils/utils_remote.gd
+++ src/utils/utils_remote.gd
@@ -1,6 +1,6 @@
 #! remote
 
-const UFile = preload("uid://gs632l1nhxaf") #! resolve ALibRuntime.Utils.UFile
+const UFile = preload("uid://bl33psa06nv1e") #! resolve ALibRuntime.Utils.UFile.Methods
 const UNode = preload("uid://dsywt12xnn7oh") #! resolve ALibRuntime.Utils.UNode
 const UTexture = preload("uid://ddu76iygjkxih") #! resolve ALibRuntime.Utils.UTexture
 
